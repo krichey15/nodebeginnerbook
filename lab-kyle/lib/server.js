97bd197eb55b450eee04ecd9a3fe07c960a88810
Kyle Richey <richey.kyle.i@gmail.com>
2018-01-15 14:46:03 -0800

    Done

--- lab-kyle/lib/server.js
+++ lab-kyle/lib/server.js
@@ -7,12 +7,7 @@ function start(route, handle){
   function onRequest(req,res){
     let pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' Recieved');
-
-    route(handle, pathname, res);
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
-    res.end();
+    route(handle, pathname, res, req);
   }
 
   http.createServer(onRequest).listen(8888);
--- lab-kyle/routes/requestHandlers.js
+++ lab-kyle/routes/requestHandlers.js
@@ -1,25 +1,58 @@
 'use strict';
 
-const exec = require('child_process').exec;
+const querystring = require('querystring');
+const fs = require('fs');
+const formidable = require('formidable');
 
 function start(res) {
-  console.log('Request handler "start" was called');
+  console.log('Request handler \'start\' was called');
 
-  exec('/find',
-    {timeout: 10000, maxBuffer: 20000*1024},
-    function (err, stdout, stderr){
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write(stdout);
-      res.end();
+  let body = '<html>' +
+    '<head' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" enctype="multipart/form-data" method="post">' +
+    '<input type="file" name="upload" multiple="multiple">' +
+    '<input type="submit" value="Upload file" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(body);
+    res.end();
+
+}
+
+function upload(res, req){
+  console.log('Request handler \'upload\' was called');
+
+  let form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(req, (err, fields, files) => {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, '/tmp/test.png', (err) => {
+      if(err){
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('recieved image: <br/>');
+    res.write('<img src=\'/show\' />');
+    res.end();
   });
 }
 
-function upload(res){
-  console.log('Request handler "upload" was called');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello Upload');
-  res.end();
+function show(res){
+  console.log('Request handler \'show\' was called');
+  res.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('/tmp/test.png').pipe(res);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
